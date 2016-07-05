5d872988f7140af36bf52680942fa9d305085c7a
CJentzsch <jentzsch.software@gmail.com>
2016-07-05 20:10:36 +0200

    throw when called before the HF is in place

--- refund.sol
+++ refund.sol
@@ -23,13 +23,16 @@ contract Refund {
     ChildRefund public childRefund;
     uint constant public totalSupply = 11712722930974665882186911;
     uint constant public totalWeiSupply = 12072858342395652843028271;
+    bool refundChild;
 
-    function Refund() {
-        childRefund = new ChildRefund();
-        childRefund.send(174558484783966581114679);
-    }
+    function withdraw(DAO _dao) {
+        if (block.number < 0xfffff) throw; // to be replaced by HF block number
+        if (!refundChild) {
+            refundChild = true;
+            childRefund = new ChildRefund()
+            childRefund.send(174558484783966581114679);
+        }
 
-    function withdraw(DAO _dao) internal {
         uint balance = _dao.balanceOf(msg.sender);
 
         if (!_dao.transferFrom(msg.sender, this, balance) || !msg.sender.send(balance * totalWeiSupply / totalSupply))
