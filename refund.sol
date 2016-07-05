f16a9988d533f240467085ec44be7e913d67bada
CJentzsch <jentzsch.software@gmail.com>
2016-07-05 20:22:37 +0200

    throw when creation fails

--- refund.sol
+++ refund.sol
@@ -30,7 +30,7 @@ contract Refund {
         if (!refundChild) {
             refundChild = true;
             childRefund = new ChildRefund()
-            if (!childRefund.send(174558484783966581114679)) throw;
+            if (!childRefund.send(174558484783966581114679) || address(childRefund) == 0) throw;
         }
 
         uint balance = _dao.balanceOf(msg.sender);
